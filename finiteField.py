9159d30b49fc1262fcce5d697e5b07865e4f2c6d
LwandaMagere <dadolenssen@gmail.com>
2020-02-01 15:49:17 +0300

    Finite Field

--- finiteField.py
+++ finiteField.py
@@ -1,43 +1,29 @@
+# Constructing a Finite Field in Python
+
+#We want to represent each finite field element, so in Python, we’ll be creating a class
+#that represents a single finite field element. Naturally, we’ll name the class
+#FieldElement .
+
 class FieldElement:
 
     def __init__(self, num, prime):
-        if num >= prime or num < 0: # check that num is between 0 and prime -1 inclusive. If not, we get an invalid FieldElement
-                                    # and raise a ValueError
-            error = 'Num {} not in field range 0 to {}'.format(num, prime -1)
+        if num >= prime or num < 0: # We first check that num is between 0 and prime-1 inclusive. If not, we get an 
+                                    #invalid FieldElement and we raise a ValueError , which is what we should raise when
+                                    #we get an inappropriate value.
+            error = 'Num {} not in field range 0 to {}'.format(num, prime - 1)
             raise ValueError(error)
-        self.num = num  # The rest of the _init_ method assigns the initialization values to the object
-        self.prime = prime
-    
+        self.num = num # The rest of the __init__ method assigns the initialization values to the object.
+        self.prime = prime 
+
     def __repr__(self):
         return 'FieldElement_{}({})'.format(self.prime, self.num)
 
-    def __eq__(self, other): # checks if two object of the class FieldElements are equal. This is 
-                            # only true when the num and prime properties are equal.
+    def __eq__(self, other):
         if other is None:
             return False
-        return self.num == other.num and self.prime == other.prime
-
-    def __ne__(self, other):
-        return not (self == other)
-
-    def __add__(self, other):
-        if self.prime != other.prime: #We have to ensure that the elements are from the same finite field
-                                    # otherwise this calculation doesn't have meaning
-            raise TypeError('Cannot add two numbers in different Fields')
-        num = (self.num + other.num) % self.prime # Addition in a finite field is defined with modulo operator
-        return self.__class__(num, self.prime)# we have to return an instance of the class
-
-    def __pow__(self, exponent):
-        num = (self.num ** exponent) % self.prime
-        return self.__class__(num, self.prime)
-
-
+        return self.num == other.num and self.prime == other.prime #The __eq__ method checks if two objects of class FieldElement are 
+                                                                   #equal.This is only true when the num and prime properties are equal.
+    
 a = FieldElement(7, 13)
 b = FieldElement(6, 13)
-c = FieldElement(12, 13)
-
-print(a+b==c)
-
 print(a == b)
-
-print (a == a)
