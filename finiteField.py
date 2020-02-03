e6f16e1798caf244f6c7bbf0c2e0092a3a64f19d
LwandaMagere <dadolenssen@gmail.com>
2020-02-03 13:03:00 +0300

    adding class point

--- finiteField.py
+++ finiteField.py
@@ -66,6 +66,34 @@ class FieldElement:
      #   num = pow(self.num, n, self.prime)
       #  return self.__class__(num, self.prime)
 
+class Point:
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b 
+        self.x = x
+        self.y = y 
+        if self.y**2 != self.x**3 + a * x + b: # We check here that the point is actually on the curve.
+            raise ValueError('({},{}) is not on the curve'.format(x,y)) 
+
+    def __eq__(self, other): #Points are equal if and only if they are on the same curve and have the same coordinates.
+        return self.x == other.x  and self.y == other. y and self.a == other.a and self.b == other.b
+    
+    def __ne__(self, other):
+        return not (self == other)
+    
+def on_curve(x, y):
+        return y**2 == x**3 + 5 * x + 7
+
+print(on_curve(2,4))
+print(on_curve(-1, -1))
+print(on_curve(5,7))
+
+
+
+p1 = Point(-1, -1, 5, 7)        
+p2 = Point(-1, -2, 5, 7)
+
+
 
 a = FieldElement(7, 13)
 b = FieldElement(6, 13)
